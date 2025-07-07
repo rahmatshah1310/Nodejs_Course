2a7575a2c1c9bba92a672d2bc1f5beb16df86620
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-07 15:58:12 +0500

    feat: Learning express js

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+const path=require('path')
 const http = require("http");
-const routes=require("./routes")
-const fs = require("fs");
-console.log(routes.someText)
-const server = http.createServer(routes.handler)
+const bodyParser=require('body-parser')
+
+const express=require("express")
+const adminRoutes=require("./routes/admin")
+const userRoutes=require("./routes/shop")
+const app=express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use('/admin',adminRoutes)
+app.use(userRoutes)
+
+app.use((req,res,next)=>{
+   res.status(404).sendFile(path.join(__dirname,'views','page-notfound.html'))
+})
+
+
+const server = http.createServer(app)
 server.listen(3000)
