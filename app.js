c398f3320984fc8ff8e1ce607ea02577224c6876
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-08 17:03:58 +0500

    feat: Adding ejs to the also use controller for products

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const path=require('path')
-const http = require("http");
-const bodyParser=require('body-parser')
+const path = require('path');
 
-const express=require("express")
-const adminRoutes=require("./routes/admin")
-const userRoutes=require("./routes/shop")
-const app=express();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+const errorController=require("./controllers/error")
 
-app.use('/admin',adminRoutes)
-app.use(userRoutes)
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(errorController.get404);
 
-app.use((req,res,next)=>{
-   res.status(404).sendFile(path.join(__dirname,'views','page-notfound.html'))
-})
-
-
-const server = http.createServer(app)
-server.listen(3000)
+app.listen(3000);
