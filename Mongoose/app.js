a4b709c8b651dac8e0210a0bcb073f57c26ba7d4
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-12 23:00:17 +0500

    adding app.js

---
+++ Mongoose/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+require("dotenv").config();
+const { mongooseConnection } = require('./config/db');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const responseMiddleware=require('./middlewares/response.mw')
+const session=require('express-session')
+
+
+app.use(
+  session({
+    secret:"secret2025",
+    resave:false,
+    saveUninitialized:false
+  })
+)
+
+//handle ejs file in the view
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'view'));
+
+//handle all middleware related to response like fail success
+app.use(responseMiddleware)
+app.use(express.static("public"));
+
+//handle all routes in the authroutes
+const authRoutes = require('./routes/auth.routes');
+app.use('/api/v1', authRoutes); 
+
+//using it for public images or styling
+app.get('/', (req, res) => {
+  res.render('index', { title: "Instagram" });
+});
+
+app.use('/api/v1',require('./routes/index'))
+
+mongooseConnection();
+
+
+//handle page not found
+app.use(function(err,req,res,next){
+  console.log("Page Not Found")
+  res.fail("Page Not Found")
+})
+
+module.exports = app;
