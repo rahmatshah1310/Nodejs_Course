c70ded0c1d3fac069d77e7f838ccd847f246283a
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-13 23:05:13 +0500

    adding response.mw.js

---
+++ Mongoose/middlewares/response.mw.js
@@ -0,0 +1,11 @@
+module.exports=(req,res,next)=>{
+    res.success=(data)=>{
+        res.status(200).json({status:'success',data})
+    }
+
+     res.fail = (data) => {  
+    res.status(400).json({ status: 'fail', data });
+  };
+
+  next()
+}
