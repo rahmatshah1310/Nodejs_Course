79edb66a27b9c703b095bbd1fd83103d1cb97693
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-14 23:38:32 +0500

    feat Adding post model

---
+++ Mongoose/model/post.model.js
@@ -0,0 +1,54 @@
+const mongoose=require("mongoose");
+
+const postShema=new mongoose.Schema({
+    PostBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        requried:true,
+    },
+    caption:{
+        type:String,
+        default:null
+    },
+    imageUrls:[
+        {
+            type:String,
+            required:true,
+        }
+    ],
+    // likes:[{
+    //     type:mongoose.Schema.Types.ObjectId,
+    //     ref:"User",
+    // }],
+    // likeCount:{
+    //     type:Number,
+    //     default:0
+    // },
+    // commentCount:{
+    //     type:Number,
+    //     default:0
+    // },
+    // isPublic:{
+    //     type:Boolean,
+    //     default:true,
+    // },
+    // comments:{
+    //     user:{
+    //         type:mongoose.Schema.Types.ObjectId,
+    //         ref:"User",
+    //         required:true,
+    //     },
+    //     comments:{
+    //         type:String,
+    //         required:true,
+    //     },
+    //     createdAt:{
+    //         type:Date,
+    //         default:Date.now,
+    //     }
+    // }
+},
+{timestamps:true}
+)
+
+module.exports=mongoose.model("Post",postShema)
