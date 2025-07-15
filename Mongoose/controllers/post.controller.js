bd7ade36c979f6d1eb079293c8182d56d29c7b83
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-15 23:53:03 +0500

    adding postcontroller

--- Mongoose/controllers/post.controller.js
+++ Mongoose/controllers/post.controller.js
@@ -26,30 +26,74 @@ module.exports.createPost=async (req,res)=>{
     }
 }
 
-module.exports.updateCaption=async (req,res)=>{
+module.exports.updateCaption = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { caption } = req.body;
+
+    if (!caption) {
+      return res.status(400).json({ message: "Caption is required" });
+    }
+    const updatedPost = await PostService.updateCaption(id, req.user._id, caption);
+
+    if (!updatedPost) {
+      return res.status(404).json({ message: "Post not found or unauthorized" });
+    }
+
+    return res.status(200).json({
+      message: "Caption updated successfully",
+      post: updatedPost,
+    });
+  } catch (error) {
+    console.error("PostController [updateCaption] Error:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+
+module.exports.updatePost=async (req,res)=>{
     try {
         const {id}=req.params;
-        const {caption}=req.body;
+        let {caption,isPublic}=req.body;
 
-        console.log(id,caption,"id and caption.......................................")
-
-        if(!caption){
-            return res.status(400).json({message:"Caption is required"})
+        if(typeof isPublic !== "boolean"){
+            return res.fail("Invalid value for isPulic.It must be Boolean (true or false)")            
         }
 
-        const updatedPost=await PostService.updateCaption(id,req.user._id,caption)
-        console.log(updatedPost,"updatedPost..................................")
+            const updatePost=await PostService.updatePost(id,{caption,isPublic});
+            if(!updatePost){
+                return res.fail("Post Not Found");
+            }
 
-        if(!updatedPost){
-            return res.status(404).json({message:"Post Not Found or unauthorized"})
-        }
-
-        return res.status(200).json({
-            message:"Caption updated Succesfully",
-            post:updatedPost,
-        })
+             return res.status(200).json({
+            message: "Post updated successfully",
+            post: updatePost
+        });
     } catch (error) {
-        console.error("PostController [updateCaption] Error",error);
+        console.error("PostController [udpatePost] Error",error)
         return res.status(500).json({message:"Internal Server Error"})
+    }
+}
+
+module.exports.deletePost=async (req,res)=>{
+    try {
+        const {id}=req.params;
+        await PostService.deletePost(id)
+        return res.success("Post Deleted Succesfully")
+    } catch (error) {
+        console.error("PostService [deletePost] Error",error)
+        return res.status(500).json({message:"Internal Server Error"})
+    }
+}
+
+
+module.exports.getPostByUser=async (req,res)=>{
+    try {
+        const userId=req.res._id;
+        const posts= await PostService.getPostsByUser(userId);
+        return res.success(posts)
+    } catch (error) {
+        console.error("PostController [getPostByUser] Error",error)
+        return res.status(500).json({message:"Internval Server Error"})
     }
 }
