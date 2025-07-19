4c2e48cf80b6584a799361e10f090777822833bf
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-19 17:34:07 +0500

    add

--- Mongoose/model/user.js
+++ Mongoose/model/user.js
@@ -1,69 +1,77 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  websiteUrl: {
+    type: String,
+    default: null,
+  },
+  gender: {
+    type: String,
+    enum: ["Male", "Female"],
+  },
+  isPulic: {
+    type: Boolean,
+    default: true,
+  },
+  profilePic: {
+    type: String,
+    default: process.env.DEFAULT_PROFILE_PICTURE || null,
+  },
+  bio: {
+    type: String,
+    default: "",
+  },
+  follower: [
     {
-        fullName: {
-            type: String,
-            required: true,
-        },
-        userName: {
-            type: String,
-            unique: true,
-            required: true
-        },
-        email: {
-            type: String,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        websiteUrl:{
-            type:String,
-            default:null,
-        },
-        gender:{
-            type:String,
-            enum:["Male","Female"]
-        },
-        isPulic:{
-            type:Boolean,
-            default:true,
-        },
-        profilePic:{
-            type:String,
-            default:process.env.DEFAULT_PROFILE_PICTURE || null
-        },
-        bio:{
-            type:String,
-            default:""
-        },
-    forgotPasswordCode: { type: Number, default: null },
-    passwordResetCodeExpiry: { type: Date, default: null },
-    }
-)
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  forgotPasswordCode: { type: Number, default: null },
+  passwordResetCodeExpiry: { type: Date, default: null },
+});
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next()
-})
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 //hash password before update
-userSchema.pre("findOneAndUpdate", async function (next){
-    const updateInfo=this.getUpdate();
-    if(updateInfo.password){
-        updateInfo.password=await bcrypt.hash(updateInfo.password,10)
-    }
-    next()
-})
-
+userSchema.pre("findOneAndUpdate", async function (next) {
+  const updateInfo = this.getUpdate();
+  if (updateInfo.password) {
+    updateInfo.password = await bcrypt.hash(updateInfo.password, 10);
+  }
+  next();
+});
 
 userSchema.methods.isValidPassword = async function (password) {
-    const compare = await bcrypt.compare(password, this.password)
-    return compare;
-}
+  const compare = await bcrypt.compare(password, this.password);
+  return compare;
+};
 
-
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
