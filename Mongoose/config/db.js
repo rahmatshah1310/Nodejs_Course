4eaf1c45bfcbb7e69a3a9586980e565f1b7bf923
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-21 17:19:14 +0500

    add

--- Mongoose/config/db.js
+++ Mongoose/config/db.js
@@ -1,11 +1,12 @@
-const mongoose=require('mongoose');
-const MONGO_URI="mongodb+srv://rahmatshah:rahmatshah@cluster0.usljxbr.mongodb.net/instagram";
-exports.mongooseConnection= async()=>{
-    try {
-        await mongoose.connect( MONGO_URI).then(data=>{
-            console.log(`Succesfully Connected to the MongoDB ${data.connection.host}`)
-        })
-    } catch (error) {
-        console.error("Unable to connect to the database",error)
-    }
-}
+const mongoose = require("mongoose");
+const MONGO_URI =
+  "mongodb+srv://rahmatshah:rahmatshah@cluster0.usljxbr.mongodb.net/instagram";
+exports.mongooseConnection = async () => {
+  try {
+    await mongoose.connect(MONGO_URI).then((data) => {
+      console.log(`Succesfully Connected to the MongoDB `);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database", error);
+  }
+};
