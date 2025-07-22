b7fb05cd7ea890aa417fc18ef5b4f6e97bb5545d
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-22 18:27:52 +0500

    add

---
+++ Mongoose/model/story.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const storySchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+storySchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+module.exports = mongoose.model("Story", storySchema);
