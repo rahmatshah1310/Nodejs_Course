f1c584c58042c7a4cca706fda4c716e160a6ea83
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-22 18:27:40 +0500

    add

---
+++ Mongoose/model/archive.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const archiveSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Archive", archiveSchema);
