04224fb5a6e981eb200be57c56d3885abf4de18b
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-22 18:27:33 +0500

    add

--- Mongoose/middlewares/upload.mw.js
+++ Mongoose/middlewares/upload.mw.js
@@ -1,23 +1,23 @@
-const multer=require('multer')
-const {CloudinaryStorage}=require('multer-storage-cloudinary')
-const cloudinary=require('../config/cloudinary.config')
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary.config");
 
-module.exports={
-    ProfilePicUpload:multer({
-       storage:new CloudinaryStorage({
-        cloudinary:cloudinary,
-        params:async (req,file)=>{
-            userEmail=req.user.email;
-            return {
-                folder:`instagram-backend/${userEmail}/profile-pic`,
-                allowed_formats:["jpg","jpeg","png"],
-                transformation:[{width:500,height:500,crop:"fill"}]
-            }
-        }
-       })
+module.exports = {
+  ProfilePicUpload: multer({
+    storage: new CloudinaryStorage({
+      cloudinary: cloudinary,
+      params: async (req, file) => {
+        userEmail = req.user.email;
+        return {
+          folder: `instagram-backend/${userEmail}/profile-pic`,
+          allowed_formats: ["jpg", "jpeg", "png"],
+          transformation: [{ width: 500, height: 500, crop: "fill" }],
+        };
+      },
     }),
+  }),
 
-    postImageUpload: multer({
+  postImageUpload: multer({
     storage: new CloudinaryStorage({
       cloudinary: cloudinary,
       params: async (req, file) => {
@@ -29,7 +29,19 @@ module.exports={
         };
       },
     }),
-  })
-}
+  }),
 
- 
+  storyImageUpload: multer({
+    storage: new CloudinaryStorage({
+      cloudinary: cloudinary,
+      params: async (req, file) => {
+        const userEmail = req.user.email;
+        return {
+          folder: `instagram-backend/${userEmail}/stories`,
+          allowed_formats: ["jpg", "jpeg", "png"],
+          transformation: [{ width: 1080, height: 1920, crop: "fill" }],
+        };
+      },
+    }),
+  }),
+};
