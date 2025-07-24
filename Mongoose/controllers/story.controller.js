4fb3c0270a37141222dfb9d7791fdc1d22430311
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-24 17:51:25 +0500

    add

--- Mongoose/controllers/story.controller.js
+++ Mongoose/controllers/story.controller.js
@@ -6,16 +6,21 @@ module.exports.uploadStory = async (req, res) => {
     const imageUrl = req.file?.path;
 
     if (!imageUrl) {
-      return res.status(400).json({ message: "Image is required" });
+      return res.status(400).json({
+        message: "Image is required",
+      });
     }
 
     const story = await StoryService.createStory(userId, imageUrl);
-    return res
-      .status(201)
-      .json({ message: "Story Created Succesfully", story });
+    return res.status(201).json({
+      message: "Story Created Succesfully",
+      story,
+    });
   } catch (error) {
     console.error("StoryController [story] Error", error);
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
   }
 };
 
@@ -24,15 +29,20 @@ module.exports.getAllArchives = async (req, res) => {
     const userId = req.user._id;
     const archives = await StoryService.getAllArchives(userId);
     if (!archives || archives.length === 0) {
-      return res.status(404).json({ message: "No Archives Found" });
+      return res.status(404).json({
+        message: "No Archives Found",
+      });
     }
 
-    return res
-      .status(200)
-      .json({ message: "Archives reterived succesfully", archives });
+    return res.status(200).json({
+      message: "Archives reterived succesfully",
+      archives,
+    });
   } catch (error) {
     console.error("StoryController [getAllArchives] Error", error);
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
   }
 };
 
@@ -41,12 +51,15 @@ module.exports.getAllStories = async (req, res) => {
     const userId = req.user._id;
     const allStories = await StoryService.getUserStories(userId);
 
-    return res
-      .status(200)
-      .json({ message: "AllStories Get succesfully", stories: allStories });
+    return res.status(200).json({
+      message: "AllStories Get succesfully",
+      stories: allStories,
+    });
   } catch (error) {
     console.error("StoryController [getAllArchives] Error", error);
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
   }
 };
 
@@ -55,13 +68,19 @@ module.exports.deleteStory = async (req, res) => {
     const { id } = req.params;
     const deleted = await StoryService.deleteStory(id, req.user._id);
     if (!deleted) {
-      return res.status(404).json({ message: "Story not found." });
+      return res.status(404).json({
+        message: "Story not found.",
+      });
     }
 
-    return res.status(200).json({ message: "Story Deleted Succesfully" });
+    return res.status(200).json({
+      message: "Story Deleted Succesfully",
+    });
   } catch (error) {
     console.error("StoryController [deleteStory] Error", error);
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
   }
 };
 
@@ -70,14 +89,19 @@ module.exports.getstoriesOfFollowing = async (req, res) => {
     const userId = req.user._id;
     const stories = await StoryService.getstoriesOfFollowing(userId);
     if (!stories || stories.length === 0) {
-      return res.status(404).json({ message: "Story not found." });
+      return res.status(404).json({
+        message: "Story not found.",
+      });
     }
 
-    return res
-      .status(200)
-      .json({ message: "Story reterived Succesfully", stories });
+    return res.status(200).json({
+      message: "Story reterived Succesfully",
+      stories,
+    });
   } catch (error) {
     console.error("StoryController [getstoriesOfFollowing] Error", error);
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
   }
 };
