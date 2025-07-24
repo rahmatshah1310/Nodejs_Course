4c841309826af41717d1ba372bd07716a0958491
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-24 17:51:30 +0500

    add

--- Mongoose/middlewares/response.mw.js
+++ Mongoose/middlewares/response.mw.js
@@ -1,11 +1,11 @@
-module.exports=(req,res,next)=>{
-    res.success=(data)=>{
-        res.status(200).json({status:'success',data})
-    }
-
-     res.fail = (data) => {  
-    res.status(400).json({ status: 'fail', data });
+module.exports = (req, res, next) => {
+  res.success = (data) => {
+    res.status(200).json({ status: "success", data });
   };
 
-  next()
-}
+  res.fail = (data) => {
+    res.status(400).json({ status: "fail", data });
+  };
+
+  next();
+};
