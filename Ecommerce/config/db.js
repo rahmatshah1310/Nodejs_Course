0af90e8c65f21fa792f3e71cb1d481d20f41b9c5
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-30 15:14:02 +0500

    add

---
+++ Ecommerce/config/db.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose");
+
+const MONGO_URI="mongodb+srv://rahmatshah:rahmatshah@cluster0.usljxbr.mongodb.net/ecommerce"
+
+exports.mongooseConnection=async()=>{
+    try {
+        await mongoose.connect(MONGO_URI).then((data)=>{
+            console.log(`Succesfully Connected to the MongoDB`)
+        })
+    } catch (error) {
+        console.error("Unable to connect to the database",error)
+    }
+}
