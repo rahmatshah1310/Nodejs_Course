92639cb5e33f871710db22874c7bf0c98f50d110
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-09-03 21:53:20 +0500

    remove

--- Ecommerce/helpers/jwt.js
+++
@@ -1,6 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-module.exports.generateToken = (user) => ({
-  token: "Bearer " + jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "60d" }),
-  user,
-});
