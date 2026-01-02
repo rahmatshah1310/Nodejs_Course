c3ed3d2b531f329c8d0a7238bf1b764bd6d1e76a
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-01-02 23:55:48 +0500

    add product.js

--- MVC/mdoels/products.js
+++ MVC/mdoels/products.js
@@ -1,21 +1,16 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-const p = path.join(
-  path.dirname(require.main.filename),
-  'data',
-  'products.json'
-);
+const p = path.join(path.dirname(require.main.filename), "data", "products.json");
 
-
-const getProductsFromFile=cb=>{
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        return cb([]);
-      }
-        cb(JSON.parse(fileContent));
-    });
-}
+// const getProductsFromFile=cb=>{
+//     fs.readFile(p, (err, fileContent) => {
+//       if (err) {
+//         return cb([]);
+//       }
+//         cb(JSON.parse(fileContent));
+//     });
+// }
 
 module.exports = class Product {
   constructor(t) {
@@ -23,15 +18,15 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile(products=>{
-         products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
-    })
-    };
+    });
+  }
 
   static fetchAllProducts(cb) {
-    getProductsFromFile(cb)
+    getProductsFromFile(cb);
   }
 };
