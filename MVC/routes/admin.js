080f15b66353463d5ffa1cce95a7a72ee8f14d33
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-01-02 23:55:38 +0500

    add admin.js

--- MVC/routes/admin.js
+++ MVC/routes/admin.js
@@ -1,15 +1,12 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const productController=require("../controllers/products")
+const productController = require("../controllers/products");
 
-router.get('/add-product', productController.getAddProducts);
-// /admin/add-product => POST
-router.post('/add-product',productController.postAddProducts);
+router.get("/add-product", productController.getAddProducts);
+router.post("/add-product", productController.postAddProducts);
 
-
-
-module.exports=router;
+module.exports = router;
